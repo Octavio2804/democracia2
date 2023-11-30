d251cf321d5322f68562c50bd97100ddf5d3a580
Octavio Olaeta <octavioolaeta28@gmail.com>
2023-11-30 15:18:02 -0300

    falta hacer capacidad de girar

--- App.js
+++ App.js
@@ -9,27 +9,10 @@ import ComoJugar from './screens/ComoJugar';
 import Jugadores from './screens/Jugadores';
 import Resultado from "./screens/Resultado"
 import Juego from './screens/Juego';
-import 'firebase/firestore';
-import firebase from 'firebase/app';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 
 
 const Stack = createStackNavigator();
-
-/* const firebaseConfig = {
-  apiKey: "AIzaSyCJyZbuWxXou9Aojh9N5l0-ng83fqsvk3k",
-  authDomain: "democracia-a45f6.firebaseapp.com",
-  projectId: "democracia-a45f6",
-  storageBucket: "democracia-a45f6.appspot.com",
-  messagingSenderId: "107206939490",
-  appId: "1:107206939490:web:5f66ba2fc89866115a523b",
-  measurementId: "G-TBJ6HL2TTZ"
-}; */
-
-/* // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app); */
 
 export default function App() {
   return (
